9a44441ef8ee66cc1fae0363fc4137cb98cedb4d
Will Brindle <will.brindle@bazaarvoice.com>
2020-03-20 08:46:37 +0000

    fix build

--- app/server.go
+++ app/server.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"fmt";
+        "os";
+        "net"
 )
 
 func main() {
-	fmt.Println("boot your server here!")
+	fmt.Println("Launching server!")
 
-	 Comment out the following to pass the first stage
-	
 	 l, err := net.Listen("tcp", "0.0.0.0:6379")
 	 if err != nil {
 	 	fmt.Println("Failed to bind to port 6379")
