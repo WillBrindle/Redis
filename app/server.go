ae5ef6075e12a27a4ca19eeac36385e98a3c977a
Will Brindle <will.brindle@bazaarvoice.com>
2020-03-20 08:31:11 +0000

    Fixed basic bind implementation

--- app/server.go
+++ app/server.go
@@ -7,16 +7,16 @@ import (
 func main() {
 	fmt.Println("boot your server here!")
 
-	// Comment out the following to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	 Comment out the following to pass the first stage
+	
+	 l, err := net.Listen("tcp", "0.0.0.0:6379")
+	 if err != nil {
+	 	fmt.Println("Failed to bind to port 6379")
+	 	os.Exit(1)
+	 }
+	 _, err = l.Accept()
+	 if err != nil {
+	 	fmt.Println("Error accepting connection: ", err.Error())
+	 	os.Exit(1)
+	 }
 }
