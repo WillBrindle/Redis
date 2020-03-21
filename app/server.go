71f63ff03895abe6ff506532489b3879fa5e2090
Will Brindle <will.brindle@bazaarvoice.com>
2020-03-21 10:15:47 +0000

    fix nano conversion

--- app/server.go
+++ app/server.go
@@ -80,14 +80,14 @@ func handleConnection(conn net.Conn) {
           key := strArr[i]
           if strings.ToUpper(key) == "PX" {
             px, _ := strconv.ParseInt(strArr[i + 1], 10, 64)
-            expTime = time.Now().UnixNano() / 1000 + px
+            expTime = time.Now().UnixNano() + px * 1000
           }
         }
 
         m[strArr[1]] = RedisRecord{strArr[2], expTime}
         conn.Write([]byte("+OK\r\n"))
       case "GET":
-        now := time.Now().UnixNano() / 1000
+        now := time.Now().UnixNano()
         res, ok := m[strArr[1]]
 
         if ok == false {
