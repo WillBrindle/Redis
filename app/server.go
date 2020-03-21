03a2c7bd4ece8f744464d1624b01f14307d7b284
Will Brindle <will.brindle@bazaarvoice.com>
2020-03-21 07:46:47 +0000

    Respond to pings

--- app/server.go
+++ app/server.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-	"fmt";
-        "os";
-        "net"
+  "fmt";
+  "os";
+  "net";
+  "io"
 )
 
-func main() {
-	fmt.Println("Launching server!")
+func handleConnection(conn net.Conn) {
+  fmt.Println("Connection accepted")
 
-	 l, err := net.Listen("tcp", "0.0.0.0:6379")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 6379")
-	 	os.Exit(1)
-	 }
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+  tmp := make([]byte, 256)
+  for {
+    _, err := conn.Read(tmp)
+    if err != nil {
+      if err != io.EOF {
+        fmt.Println("read error:", err)
+      }
+      break
+     }
+    
+     conn.Write([]byte("+PONG\r\n"))
+  }
+
+  conn.Close()
+}
+
+func main() {
+  fmt.Println("Launching server!")
+
+  l, err := net.Listen("tcp", "0.0.0.0:6379")
+  if err != nil {
+    fmt.Println("Failed to bind to port 6379")
+    os.Exit(1)
+  }
+	 
+  for {
+    conn, err := l.Accept()
+    if err != nil {
+      fmt.Println("Error accepting connection: ", err.Error())
+      os.Exit(1)
+    }
+
+    go handleConnection(conn)
+  }
 }
