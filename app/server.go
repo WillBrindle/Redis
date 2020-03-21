d10b177d23b3b1cbebde8bd28241bf7350fdd0fa
Will Brindle <will.brindle@bazaarvoice.com>
2020-03-21 08:52:46 +0000

    Primitive echo command handling

--- app/server.go
+++ app/server.go
@@ -4,13 +4,32 @@ import (
   "fmt";
   "os";
   "net";
-  "io"
+  "io";
+  "bytes";
+  "strconv"
 )
+
+func readChunk(b *bytes.Buffer) []byte {
+  c := make([]byte, 0, b.Len())
+  read := 0
+  for {
+    p := b.Bytes()
+    if bytes.Equal(p[:2], []byte("\r\n")) {
+      break
+    }
+    c = append(c, b.Next(1)...)
+    read ++
+  }
+
+  b.Next(2)
+
+  return c[0:read]
+}
 
 func handleConnection(conn net.Conn) {
   fmt.Println("Connection accepted")
 
-  tmp := make([]byte, 256)
+  tmp := make([]byte, 1024)
   for {
     _, err := conn.Read(tmp)
     if err != nil {
@@ -18,9 +37,36 @@ func handleConnection(conn net.Conn) {
         fmt.Println("read error:", err)
       }
       break
-     }
-    
-     conn.Write([]byte("+PONG\r\n"))
+    }
+
+    b := bytes.NewBuffer(tmp)
+    msgStart := readChunk(b)
+
+    if msgStart[0] == byte('*') {
+      elems, err := strconv.Atoi(string(msgStart[1:]))
+      if err != nil {
+        fmt.Println("Conversion error")
+        break
+      }
+
+      strArr := make([]string, elems)
+      for i := 0; i < elems; i++ {
+        // TODO: handle long commands & overflows; will need length for that but can ignore for now
+        readChunk(b)
+        strArr[i] = string(readChunk(b))
+      }
+
+      fmt.Println("Hmm...", strArr[0])
+
+      switch cmd := strArr[0]; cmd {
+      case "PING":
+        conn.Write([]byte("+PONG\r\n"))
+      case "ECHO":
+        conn.Write([]byte("+" + strArr[1] + "\r\n"))
+      case "COMMAND":
+        conn.Write([]byte("+PONG\r\n"))
+      }
+    }
   }
 
   conn.Close()
